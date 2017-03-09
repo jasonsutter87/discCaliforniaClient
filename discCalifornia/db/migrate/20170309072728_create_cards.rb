392cfe873f586829134776cb3126bf0a0be10850
Jason Sutter <jasonsutter@Rocko.wavecable.com>
2017-03-08 23:48:34 -0800

    updated card migration

--- discCalifornia/db/migrate/20170309072728_create_cards.rb
+++ discCalifornia/db/migrate/20170309072728_create_cards.rb
@@ -4,7 +4,6 @@ class CreateCards < ActiveRecord::Migration[5.0]
       t.string :course_name
       t.string :player_name
       t.integer :hole_count
-
       t.integer :course_id
       t.integer :player_id
       t.timestamps
