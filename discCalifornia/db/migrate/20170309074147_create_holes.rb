0171e83aa28fb2ea8628cc9de6e2ce1cec0fb079
Jason Sutter <jasonsutter@Rocko.wavecable.com>
2017-03-08 23:48:09 -0800

    write hole migrtation

--- discCalifornia/db/migrate/20170309074147_create_holes.rb
+++ discCalifornia/db/migrate/20170309074147_create_holes.rb
@@ -1,7 +1,10 @@
 class CreateHoles < ActiveRecord::Migration[5.0]
   def change
     create_table :holes do |t|
-
+      t.string :placement
+      t.integer :distance
+      t.integer :par
+      t.integer :stroke_count
       t.timestamps
     end
   end
