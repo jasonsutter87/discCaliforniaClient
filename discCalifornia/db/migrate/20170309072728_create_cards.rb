c801123ea49a6fc40ea5d62317908f89ec205787
Jason Sutter <jasonsutter@Rocko.wavecable.com>
2017-03-08 23:42:45 -0800

    Created blank hole model and controller

--- discCalifornia/db/migrate/20170309072728_create_cards.rb
+++ discCalifornia/db/migrate/20170309072728_create_cards.rb
@@ -1,7 +1,12 @@
 class CreateCards < ActiveRecord::Migration[5.0]
   def change
     create_table :cards do |t|
+      t.string :course_name
+      t.string :player_name
+      t.integer :hole_count
 
+      t.integer :course_id
+      t.integer :player_id
       t.timestamps
     end
   end
---
+++ discCalifornia/db/migrate/20170309074147_create_holes.rb
@@ -0,0 +1,8 @@
+class CreateHoles < ActiveRecord::Migration[5.0]
+  def change
+    create_table :holes do |t|
+
+      t.timestamps
+    end
+  end
+end
